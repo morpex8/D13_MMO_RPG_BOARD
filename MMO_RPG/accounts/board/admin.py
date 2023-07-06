144748551613515558c694ce318f48fc52e1d3d2
Soulfox <97559525+morpex8@users.noreply.github.com>
2023-07-06 18:35:05 +0300

    Delete MMO_RPG/accounts/board directory

--- MMO_RPG/accounts/board/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'text', 'author', 'dateCreation']
-    list_filter = ('author', 'dateCreation')
-
-
-admin.site.register(Post, PostAdmin)
